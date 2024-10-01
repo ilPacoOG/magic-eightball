758d0fff3882b20c586406c7b1703203a826c91a
kaneganteng <kaneesasta@gmail.com>
2024-10-01 14:52:56 -0400

    changed README, added an asset folder for the css file, added Rani's html,css,and js, changed the name of person2 file into person2.html

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//rani
+document.getElementById('submit-btn').addEventListener('click', function() {
+    // Capture the user input
+    let question = document.getElementById('question-input').value;
+
+    // Trigger an answer generator (for now, just a simple response)
+    if (question.trim() !== "") {
+        document.getElementById('response').innerText = "You asked: " + question;
+    } else {
+        document.getElementById('response').innerText = "Please type a question.";
+    }
+
+    // Clear the input field
+    document.getElementById('question-input').value = '';
+});
+
+//Michael F.
 let answers = [{
     answer: 'I doubt it.',
     theme: 'negative'
@@ -38,3 +55,24 @@ let answers = [{
     theme: 'nuetral',
 }
 ]
+
+//changes by Kane
+// can possibly be used for the theme output function
+
+// const outputElement = document.getElementById("output");
+
+// function applyTheme(theme) {
+//     outputElement.classList.remove("light-theme", "dark-theme");
+//     outputElement.classList.add (theme);
+// }
+
+// const theme = positive; //may need to replace this depending on our condition
+
+// if (condition) {
+//     applyTheme("dark-theme");
+// } else {
+//     applyTheme("light-theme");
+// }
+
+//end of changes by kane
+
