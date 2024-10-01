bbaa9b9b75fc4e36ab6fd01f38337d9e781b37d5
Michael Fleming <mikahl171@gmail.com>
2024-10-01 11:33:24 -0500

    updated script for project

---
+++ script.js
@@ -0,0 +1,40 @@
+let answers = [{
+    answer: 'I doubt it.',
+    theme: 'negative'
+},
+{
+    answer: 'The starts are aligned',
+    theme: 'positive',
+},
+{    
+    answer: 'The bones say no.',
+    theme: 'negative',
+},
+{    
+    answer: 'You betcha!',
+    theme: 'positive',
+},
+{    
+    answer: 'Why not?!',
+    theme: 'nuetral',
+},
+{
+    answer: 'Check your horoscope.',
+    theme: 'nuetral',
+},
+{   answer: 'Sure',
+    theme: 'positive',
+},
+{
+    answer: 'Without a doubt!',
+    theme: 'nuetral',
+},
+{   
+    answer: 'Chances are slim.',
+    theme: 'negative',
+},
+{
+    answer: 'What does your conscience say?',
+    theme: 'nuetral',
+}
+]
